1fa1c710c408d2132c8d2c95c445964c449bd0f5
David <daviddebrun@avidart13.fr>
2017-11-24 09:27:48 +0100

    Add files via upload

--- Exercice3.php
+++ Exercice3.php
@@ -1,17 +1,18 @@
 <?php
-function note($nbreponses) {
-  if ($nbreponses > 0 && $nbreponses <= 10) {
-      return 'E';}
-  elseif ($nbreponses > 11 && $nbreponses <= 20) {
-      return 'D';}
-  elseif($nbreponses > 21 && $nbreponses <= 30) {
-      return 'C';}
-  elseif ($nbreponses > 31 && $nbreponses <= 40) {
-      return 'B';}
-  elseif ($nbreponses > 41 && $nbreponses <= 50) {
-      return 'A';}
+function plusFroid($temperatures){
+	$t = $temperatures[0];
+
+	for($i = 1; $i <=5 ; $i++):
+		if($temperatures[$i] < $t):
+			$t = $temperatures[$i];
+		endif;
+	endfor;
+
+	return $t;
 }
 
-echo note(15);
+$temperatures = [10, 12.6, 11, -1, 19, 9.3];
+
+echo plusFroid($temperatures);
 
 ?>
